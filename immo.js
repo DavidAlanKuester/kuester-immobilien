71c9602323d66efb4f5f74a354934bc21681e725
David Küster <davidalankuester@yahoo.de>
2020-08-25 15:38:42 +0200

    storyfixes + imprint first version

--- immo.js
+++ immo.js
@@ -1,5 +1,5 @@
-let i = 1;
-let img_interval = setInterval(switchPic, 10000);
+let i = 0;
+let imgInterval = setInterval(switchPic, 10000);
 
 function init() {
     window.onscroll = function () {
@@ -18,21 +18,22 @@ function init() {
             document.getElementById('menu-white').classList.remove('hide');
         }
     }
+    img(0);
 }
 
-function responsive_menu() {
-    document.getElementById('responsive-menu-click').classList.add('hide-menu');
-    document.getElementById('blackbendjava').classList.remove('hide');
-}
-
-function responsive_menu_exit() {
+function showMenu() {
     document.getElementById('responsive-menu-click').classList.remove('hide-menu');
-    document.getElementById('blackbendjava').classList.add('hide');
+    document.getElementById('blackbendjava').classList.remove('d-none');
+}
+
+function removeMenu() {
+    document.getElementById('responsive-menu-click').classList.add('hide-menu');
+    document.getElementById('blackbendjava').classList.add('d-none');
 }
 
 function resetInterval() {
     clearInterval(img_interval);
-    img_interval = setInterval(switchPic, 10000);
+    imgIinterval = setInterval(switchPic, 10000);
 }
 
 
@@ -40,36 +41,35 @@ function switchPic() {
 
     i = i + 1;
 
-    if (i > 4) {
-        i = 1;
+    if (i > 3) {
+        i = 0;
     }
     img(i);
 }
 
 function img(a) {
     document.getElementById('headline').classList.add('fly-in');
-    document.getElementById('underscore').classList.add('fly-in');
 
-    if (a == 1) {
+    if (a == 0) {
         document.getElementById('haus1').classList.remove('hide');
         document.getElementById('haus2').classList.add('hide');
         document.getElementById('haus3').classList.add('hide');
         document.getElementById('haus4').classList.add('hide');
         document.getElementById('headline').innerHTML = 'Küster Immobilien';
 
-    } else if (a == 2) {
+    } else if (a == 1) {
         document.getElementById('haus1').classList.add('hide');
         document.getElementById('haus2').classList.remove('hide');
         document.getElementById('haus3').classList.add('hide');
         document.getElementById('haus4').classList.add('hide');
         document.getElementById('headline').innerHTML = 'Handwerksservice';
-    } else if (a == 3) {
+    } else if (a == 2) {
         document.getElementById('haus1').classList.add('hide');
         document.getElementById('haus2').classList.add('hide');
         document.getElementById('haus3').classList.remove('hide');
         document.getElementById('haus4').classList.add('hide');
         document.getElementById('headline').innerHTML = 'Verwaltung';
-    } else if (a == 4) {
+    } else if (a == 3) {
         document.getElementById('haus1').classList.add('hide');
         document.getElementById('haus2').classList.add('hide');
         document.getElementById('haus3').classList.add('hide');
@@ -79,6 +79,5 @@ function img(a) {
 
     setTimeout(function () {
         document.getElementById('headline').classList.remove('fly-in');
-        document.getElementById('underscore').classList.remove('fly-in');
     }, 1000);
 }
